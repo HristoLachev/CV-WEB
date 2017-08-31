c928f55f6ea9094d9eb9fbe65c0e74f18acfec02
32043 <32043@32043-PC>
2017-09-01 00:51:47 +0300

    final

--- src/main/java/com/cv/controllers/FileDownloadController.java
+++ src/main/java/com/cv/controllers/FileDownloadController.java
@@ -8,30 +8,43 @@ import java.nio.file.Files;
 import java.nio.file.Path;
 
 import javax.servlet.http.HttpServletResponse;
+import javax.xml.bind.JAXBException;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.ResponseBody;
 
+import com.cv.converter.JAXBConverter;
+import com.cv.model.xml.SkillsPassport;
+import com.cv.serializers.XMLGregorianCalendarConverter;
 import com.google.common.base.Charsets;
+import com.google.gson.Gson;
 
 @Controller
 public class FileDownloadController {
 
+	@Autowired
+	private JAXBConverter converter;
+
 	@PostMapping("/download")
-	public void downloadXML(@RequestBody String event,HttpServletResponse response) {
+	@ResponseBody
+	public String downloadXML(@RequestBody String event, HttpServletResponse response) {
 
 		// @RequestBody
 		// String event = "wat";
 		File xml = new File("src/main/resources/cv.xml");
-//		File f=new File("C:/Work/Eclipse workspace/CV-WEB/src/main/resources/cv.xml");
-
+		// File f=new File("C:/Work/Eclipse
+		// workspace/CV-WEB/src/main/resources/cv.xml");
+		Gson gson = XMLGregorianCalendarConverter.getGson();
+		SkillsPassport skillsPassport = gson.fromJson(event, SkillsPassport.class);
 		try {
-			com.google.common.io.Files.write(event, xml, Charsets.UTF_8);
-		} catch (IOException e) {
+			converter.marshallEuroPassXML(skillsPassport, xml);
+		} catch (JAXBException e1) {
 			// TODO Auto-generated catch block
-			e.printStackTrace();
+			e1.printStackTrace();
 		}
 
 		Path file = xml.toPath();
@@ -45,5 +58,7 @@ public class FileDownloadController {
 				ex.printStackTrace();
 			}
 		}
+		return "src/main/resources/cv.xml";
+
 	}
 }
--- src/main/java/com/cv/controllers/UploadController.java
+++ src/main/java/com/cv/controllers/UploadController.java
@@ -12,6 +12,9 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RequestPart;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
@@ -31,12 +34,13 @@ public class UploadController {
 	}
 
 	@PostMapping("/upload")
-	public String handleFileUpload(@RequestParam("file") MultipartFile file, RedirectAttributes redirectAttributes) {
+	@ResponseBody
+	public SkillsPassport handleFileUpload(@RequestParam("file") MultipartFile file, RedirectAttributes redirectAttributes) {
 		if (!file.getContentType().contains("xml")) {
 			redirectAttributes.addFlashAttribute("message",
 					"File: " + file.getOriginalFilename() + " must be a xml file!");
 
-			return "redirect:/upload";
+			//return "redirect:/upload";
 		}
 
 		SkillsPassport passport = null;
@@ -52,6 +56,6 @@ public class UploadController {
 
 		String passportInJSON = XMLGregorianCalendarConverter.getGson().toJson(passport);
 		redirectAttributes.addFlashAttribute("file", passportInJSON);
-		return "redirect:/";
+		return passport;
 	}
 }
